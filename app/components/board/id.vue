1c4eb1461ed5f535d00f5091a1f4777411da6d55
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2026-03-25 00:15:06 +0900

    상세페이지 추가

--- app/components/board/id.vue
+++ app/components/board/id.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type { FormSubmitEvent } from "@nuxt/ui";
 const props = defineProps<{
   id: number;
 }>();
@@ -7,7 +8,7 @@ const user = useSupabaseUser();
 
 const { data: board } = await useAsyncData("board", async () => {
   const { data } = await client.from("Board").select("*").eq("id", props.id);
-  return data?.[0];
+  return data?.[0] ?? null;
 });
 
 const isOwner = computed(() => {
@@ -17,14 +18,40 @@ const isOwner = computed(() => {
 const handleDelete = async () => {
   const { error } = await client.from("Board").delete().eq("id", props.id);
   if (error) console.error(error);
-  navigateTo("/board/card");
+  navigateTo(`/board/${board.value?.genre.toLowerCase()}`);
+};
+
+const onSubmit = async (e: FormSubmitEvent<typeof board.value>) => {
+  if (!user.value?.sub) {
+    return;
+  }
+  const { error } = await client
+    .from("Board")
+    .update({
+      title: e.data?.title ?? "",
+      content: e.data?.content ?? "",
+    })
+    .eq("id", props.id);
+  if (error) return console.error(error);
+  navigateTo(`/board/${board.value?.genre.toLowerCase()}`);
 };
 </script>
 
 <template>
-  <div>
-    <h1>{{ board?.title }}</h1>
-    <p>{{ board?.content }}</p>
-    <UButton v-if="isOwner" @click="handleDelete">삭제</UButton>
+  <div
+    class="w-full h-full flex flex-col gap-4 items-center justify-center"
+    v-if="board"
+  >
+    <h1>{{ board.title }}</h1>
+    <UForm :state="board" @submit="onSubmit">
+      <UFormField label="제목" name="title">
+        <UInput v-model="board.title" placeholder="제목을 입력해주세요." />
+      </UFormField>
+      <UFormField label="내용" name="content">
+        <UInput v-model="board.content" placeholder="내용을 입력해주세요." />
+      </UFormField>
+      <UButton type="submit" label="저장" />
+      <UButton v-if="isOwner" @click="handleDelete" label="삭제" />
+    </UForm>
   </div>
 </template>
--- app/pages/board/card/[id].vue
+++ app/pages/board/card/[id].vue
@@ -2,7 +2,11 @@
 const route = useRoute();
 const id = Number(route.params.id);
 if (isNaN(id)) {
-  throw createError({ statusCode: 404, statusMessage: "Not Found" });
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Not Found",
+    fatal: true,
+  });
 }
 </script>
 
